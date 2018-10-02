19efd89e9375f3b15fd8c1ccca7e3f0330bd1cba
andrewstagnaro <42977270+andrewstagnaro@users.noreply.github.com>
2018-10-02 12:51:13 -0400

    Add files via upload

---
+++ Dice/src/Test.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ * @author andrew stagnaro
+ *
+ */
+public class Test 
+{
+
+	public static void main(String[] args)
+	{
+		Dice die1 = new Dice();
+		System.out.println(die1.roll());
+		System.out.println(die1.roll());
+		System.out.println(die1.roll());
+		System.out.println(die1.roll());
+		System.out.println(die1.roll());
+		System.out.println(die1.roll());
+		System.out.println(die1.roll());
+		System.out.println(die1.roll());
+		System.out.println(die1.roll());
+		System.out.println(die1.roll());
+		System.out.println(die1.roll());
+		System.out.println(die1.roll());
+		
+		System.out.println("rolls; " + die1.getNumRolls());
+		
+		
+	  
+	  
+
+	}
+
+}
